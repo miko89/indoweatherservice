1f93c1fa434ec2563e1d82d5343e4fa03194878c
miko89 <rakayasi.miko@gmail.com>
2021-07-06 15:48:11 +0700

    update

---
+++ cuaca.js
@@ -0,0 +1,28 @@
+var dataPoints_cuaca =
+[
+{ label: "00", y: [15, 26], cuaca: "1" },
+{ label: "01", y: [15, 27], cuaca: "2" },
+{ label: "02", y: [13, 27], cuaca: "3" },
+{ label: "03", y: [14, 27], cuaca: "4" },
+{ label: "04", y: [15, 26], cuaca: "5"},
+{ label: "05", y: [17, 26], cuaca: "7" },
+{ label: "06", y: [16, 27], cuaca: "6" },
+{ label: "07", y: [16, 29], cuaca: "6" },
+{ label: "08", y: [18, 30], cuaca: "3" },
+{ label: "09", y: [16, 27], cuaca: "4" },
+{ label: "10", y: [20, 28], cuaca: "2" },
+{ label: "11", y: [18, 32], cuaca: "1" },
+{ label: "12", y: [16, 27], cuaca: "3" },
+{ label: "13", y: [16, 28], cuaca: "4" },
+{ label: "14", y: [16, 27], cuaca: "5" },
+{ label: "15", y: [17, 29], cuaca: "6" },
+{ label: "16", y: [18, 27], cuaca: "1" },
+{ label: "17", y: [16, 28], cuaca: "7" },
+{ label: "18", y: [16, 27], cuaca: "1" },
+{ label: "19", y: [19, 29], cuaca: "2" },
+{ label: "20", y: [15, 30], cuaca: "5" },
+{ label: "21", y: [17, 27], cuaca: "7" },
+{ label: "22", y: [18, 29], cuaca: "6" },
+{ label: "23", y: [16, 27], cuaca: "4" }
+
+]
