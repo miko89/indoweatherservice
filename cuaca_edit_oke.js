d63d179f85c0da7ac9e184ca3481813c7f08c931
miko89 <rakayasi.miko@gmail.com>
2021-08-21 05:58:21 +0700

    oke

---
+++ cuaca_edit_oke.js
@@ -0,0 +1,28 @@
+var dataPoints_cuaca =
+[
+    { label: "00", y: [15, 26], cuaca: "1", kelurahan: "Mekarmukti"},
+    { label: "01", y: [15, 27], cuaca: "2", kelurahan: "Mekarmukti"},
+    { label: "02", y: [13, 27], cuaca: "3", kelurahan: "Mekarmukti"},
+    { label: "03", y: [14, 27], cuaca: "4", kelurahan: "Mekarmukti"},
+    { label: "04", y: [15, 26], cuaca: "5", kelurahan: "Mekarmukti"},
+    { label: "05", y: [17, 26], cuaca: "7", kelurahan: "Mekarmukti"},
+    { label: "06", y: [16, 27], cuaca: "6", kelurahan: "Mekarmukti"},
+    { label: "07", y: [16, 29], cuaca: "6", kelurahan: "Mekarmukti"},
+    { label: "08", y: [18, 30], cuaca: "3", kelurahan: "Mekarmukti"},
+    { label: "09", y: [16, 27], cuaca: "4", kelurahan: "Mekarmukti"},
+    { label: "10", y: [20, 28], cuaca: "2", kelurahan: "Mekarmukti"},
+    { label: "11", y: [18, 32], cuaca: "1", kelurahan: "Mekarmukti"},
+    { label: "12", y: [16, 27], cuaca: "3", kelurahan: "Mekarmukti"},
+    { label: "13", y: [16, 28], cuaca: "4", kelurahan: "Mekarmukti"},
+    { label: "14", y: [16, 27], cuaca: "5", kelurahan: "Mekarmukti"},
+    { label: "15", y: [17, 29], cuaca: "6", kelurahan: "Mekarmukti"},
+    { label: "16", y: [18, 27], cuaca: "1", kelurahan: "Mekarmukti"},
+    { label: "17", y: [16, 28], cuaca: "7", kelurahan: "Mekarmukti"},
+    { label: "18", y: [16, 27], cuaca: "1", kelurahan: "Mekarmukti"},
+    { label: "19", y: [19, 29], cuaca: "2", kelurahan: "Mekarmukti"},
+    { label: "20", y: [15, 30], cuaca: "5", kelurahan: "Mekarmukti"},
+    { label: "21", y: [17, 27], cuaca: "7", kelurahan: "Mekarmukti"},
+    { label: "22", y: [18, 29], cuaca: "6", kelurahan: "Mekarmukti"},
+    { label: "23", y: [16, 27], cuaca: "4", kelurahan: "Mekarmukti"}
+    
+    ]
---
+++ cuaca_editsaja.js
@@ -0,0 +1,67 @@
+var dataPoints = {
+
+
+"dps1": 
+    [
+        {label: 0, "y": 30.4, "cuaca": 1, "kelurahan": "Mekarmukti" },
+        {label: 1, "y": 28.6, "cuaca": 2, "kelurahan": "Mekarmukti" },
+        {label: 2, "y": 32.8, "cuaca": 3, "kelurahan": "Mekarmukti" },
+        {label: 3, "y": 27.6, "cuaca": 4, "kelurahan": "Mekarmukti" },
+        {label: 4, "y": 28.4, "cuaca": 5, "kelurahan": "Mekarmukti" },
+        {label: 5, "y": 31.8, "cuaca": 7, "kelurahan": "Mekarmukti" },
+        {label: 6, "y": 32.6, "cuaca": 6, "kelurahan": "Mekarmukti" },
+        {label: 7, "y": 29.2, "cuaca": 6, "kelurahan": "Mekarmukti" },
+        {label: 8, "y": 30.4, "cuaca": 3, "kelurahan": "Mekarmukti" },
+        {label: 9, "y": 30.2, "cuaca": 4, "kelurahan": "Mekarmukti" },
+        {label: 10, "y": 28.2, "cuaca": 2, "kelurahan": "Mekarmukti" },
+        {label: 11, "y": 29.0, "cuaca": 1, "kelurahan": "Mekarmukti" },
+        {label: 12, "y": 30.0, "cuaca": 3, "kelurahan": "Mekarmukti" },
+        {label: 13, "y": 32.2, "cuaca": 4, "kelurahan": "Mekarmukti" },
+        {label: 14, "y": 31.2, "cuaca": 5, "kelurahan": "Mekarmukti" },
+        {label: 15, "y": 30.8, "cuaca": 6, "kelurahan": "Mekarmukti" },
+        {label: 16, "y": 29.8, "cuaca": 1, "kelurahan": "Mekarmukti" },
+        {label: 17, "y": 31.0, "cuaca": 7, "kelurahan": "Mekarmukti" },
+        {label: 18, "y": 30.6, "cuaca": 1, "kelurahan": "Mekarmukti" },
+        {label: 19, "y": 30.8, "cuaca": 2, "kelurahan": "Mekarmukti" },
+        {label: 20, "y": 31.0, "cuaca": 5, "kelurahan": "Mekarmukti" },
+        {label: 21, "y": 28.2, "cuaca": 7, "kelurahan": "Mekarmukti" },
+        {label: 22, "y": 32.2, "cuaca": 6, "kelurahan": "Mekarmukti" },
+        {label: 23, "y": 31.2, "cuaca": 4, "kelurahan": "Mekarmukti" },
+
+  
+    ],
+
+    "dps2": 
+        [
+            {label: 0, "y": 24.4, "cuaca": 3, "kelurahan": "Mekarmukti" },
+            {label: 1, "y": 32.0, "cuaca": 3, "kelurahan": "Mekarmukti" },
+            {label: 2, "y": 31.8, "cuaca": 2, "kelurahan": "Mekarmukti" },
+            {label: 3, "y": 27.6, "cuaca": 1, "kelurahan": "Mekarmukti" },
+            {label: 4, "y": 26.0, "cuaca": 2, "kelurahan": "Mekarmukti" },
+            {label: 5, "y": 31.8, "cuaca": 3, "kelurahan": "Mekarmukti" },
+            {label: 6, "y": 32.6, "cuaca": 1, "kelurahan": "Mekarmukti" },
+            {label: 7, "y": 29.2, "cuaca": 2, "kelurahan": "Mekarmukti" },
+            {label: 8, "y": 30.2, "cuaca": 4, "kelurahan": "Mekarmukti" },
+            {label: 9, "y": 30.4, "cuaca": 5, "kelurahan": "Mekarmukti" },
+            {label: 10, "y": 28.2, "cuaca": 7, "kelurahan": "Mekarmukti" },
+            {label: 11, "y": 26.0, "cuaca": 7, "kelurahan": "Mekarmukti" },
+            {label: 12, "y": 38.0, "cuaca": 3, "kelurahan": "Mekarmukti" },
+            {label: 13, "y": 32.2, "cuaca": 2, "kelurahan": "Mekarmukti" },
+            {label: 14, "y": 30.2, "cuaca": 1, "kelurahan": "Mekarmukti" },
+            {label: 15, "y": 30.8, "cuaca": 2, "kelurahan": "Mekarmukti" },
+            {label: 16, "y": 30.8, "cuaca": 3, "kelurahan": "Mekarmukti" },
+            {label: 17, "y": 34.0, "cuaca": 4, "kelurahan": "Mekarmukti" },
+            {label: 18, "y": 30.6, "cuaca": 2, "kelurahan": "Mekarmukti" },
+            {label: 19, "y": 30.8, "cuaca": 1, "kelurahan": "Mekarmukti" },
+            {label: 20, "y": 31.0, "cuaca": 2, "kelurahan": "Mekarmukti" },
+            {label: 21, "y": 28.2, "cuaca": 3, "kelurahan": "Mekarmukti" },
+            {label: 22, "y": 35.2, "cuaca": 2, "kelurahan": "Mekarmukti" },
+            {label: 23, "y": 31.2, "cuaca": 1, "kelurahan": "Mekarmukti" },
+        ],
+    
+        
+    
+    
+    }
+
+    
